2551e1a844a09268b3b600bee3443e74865c7c60
Swapnil Borkar <BorkarS@na.int.grp>
2019-06-04 22:21:15 -0400

    commit child event

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -26,8 +26,7 @@ export class HomeComponent implements OnInit {
     });
   }
   cancelRegisterMode(registerMode:boolean){
-    debugger;
-    console.log('inside cancelRegister mode');
+
     this.registermode = registerMode;
   }
 }
